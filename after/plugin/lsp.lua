713b0d05177720f5b6a014d1a18aad96a3c4b540
Joao Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2023-12-09 09:01:50 -0300

    :fix: corrige selecao do auto complete

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,7 +5,8 @@ local cmp_action = require('lsp-zero').cmp_action()
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
     -- `Enter` key to confirm completion
-    ['<C-y>'] = cmp.mapping.confirm({select = false}),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
 
     -- Ctrl+Space to trigger completion menu
     ['<C-Space>'] = cmp.mapping.complete(),
