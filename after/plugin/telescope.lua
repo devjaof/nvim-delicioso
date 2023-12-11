ea4d167abaad4f3c88db043ac366b4ea7a67bc6b
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2023-12-11 09:43:35 -0300

    remap do go to definition :bug:

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,3 +5,5 @@ vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function() 
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, {});
