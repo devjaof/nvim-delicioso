85308565520bb8809bf4baeeb98c46fbf6f312e5
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2023-12-18 14:08:15 -0300

    usa as config default para as marcações de indentação

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -1,12 +1,1 @@
-local highlight = {
-    "RainbowGray",
-}
-
-local hooks = require "ibl.hooks"
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowGray", { fg = "#808080" })
-end)
-
-require("ibl").setup { indent = { highlight = highlight } }
+require("ibl").setup {}
