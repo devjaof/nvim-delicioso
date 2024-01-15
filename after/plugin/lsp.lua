af8cd8134ab82bf6e69c36c14939feaba1d17763
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2024-01-15 07:42:22 -0300

    melhoria em plugins lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,12 @@
-
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  }, 
   mapping = cmp.mapping.preset.insert({
     -- `Enter` key to confirm completion
     ['<CR>'] = cmp.mapping.confirm({select = false}),
@@ -18,17 +22,24 @@ cmp.setup({
     -- Scroll up and down in the completion documentation
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  })
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+  },
 })
 
 local lspconfig = require('lspconfig')
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 vim.api.nvim_create_autocmd('LspAttach', {
+  pattern = { '*.tsx', '*.ts', '*.jsx', '*.js' },
   desc = 'LSP actions',
   callback = function(event)
 	  local opts = {buffer = bufnr, remap = false}
 
+    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
 	  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -42,7 +53,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end
 })
 
-lspconfig.tsserver.setup({capabilities = lsp_capabilities, filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" }})
+lspconfig.tsserver.setup({
+  capabilities = lsp_capabilities, 
+  filetypes = { 
+    "typescript", 
+    "typescriptreact", 
+    "typescript.tsx", 
+    "javascript", 
+    "javascriptreact", 
+    "javascript.jsx" 
+  }
+})
 lspconfig.eslint.setup({capabilities = lsp_capabilities})
 
 vim.api.nvim_create_autocmd('BufWritePre', {
