7dc4635b5f94f91eb49667f5c5d0379e3b00fdcb
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2024-01-18 14:16:35 -0300

    corrige eslint config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -89,16 +89,17 @@ eslint.setup({
   bin = 'eslint',
   code_actions = {
     enable = true,
-    diagnostics = true,
     apply_on_save = {
       enable = true,
-      types = { "directive", "problem", "suggestion", "layout" }
-    }
+    },
+    disable_rule_comment = {
+      enable = true,
+      location = "separate_line"
+    },
   },
   diagnostics = {
     enable = true,
     report_unused_disable_directives = false,
     run_on = "save"
   },
-  diagnostics_format = "[eslint] #{m}\n(#{c})",
 })
