f4d9914cb7640cfefc422195eb13bda131586e04
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2024-01-18 14:07:45 -0300

    melhora config treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,10 +1,38 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "go", "javascript", "typescript", "lua", "vim" },
+  ensure_installed = { 
+    "go",
+    "tsx",
+    "json",
+    "html",
+    "css",
+    "lua",
+    "javascript", 
+    "typescript", 
+    "lua", 
+    "vim"
+  },
   sync_install = false,
   auto_install = true,
 
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
+    disable = {},
+  },
+
+  indent = {
+    enable = true,
+    disable = {}
+  },
+
+  autopairs = {
+    enable = true
+  },
+
+  autotag = {
+    enable = true
   },
 }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
