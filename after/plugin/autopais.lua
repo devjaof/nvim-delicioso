e0debfa489906dc326ba726005197aeac98179cf
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2024-01-18 14:08:00 -0300

    autopairs e autotags

---
+++ after/plugin/autopais.lua
@@ -0,0 +1,8 @@
+local autotag = require("nvim-ts-autotag")
+autotag.setup({})
+
+local autopairs = require("nvim-autopairs")
+autopairs.setup({
+  disable_filetype = { "TelescopePrompt" , "vim" },
+})
+
