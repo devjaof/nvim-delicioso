ef95042c184e251d225d88fd0c01149b9b826e5d
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2024-01-19 07:39:54 -0300

    atualiza coloschemes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,3 +1,14 @@
+require("rose-pine").setup({
+  variant = "moon",
+  styles = {
+    bold = true,
+    italic = true,
+    transparency = false
+  },
+  headings = "subtle"
+})
+
+
 function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
--- lua/ja1/packer.lua
+++ lua/ja1/packer.lua
@@ -13,9 +13,9 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
-	use({ 'rose-pine/neovim', as = 'rose-pine', config = function() 
-		vim.cmd('colorscheme rose-pine')
-	end })
+	use({ 'rose-pine/neovim', as = 'rose-pine' })
+  
+  use({ 'rebelot/kanagawa.nvim', as = 'kanagawa' })
 
 	use( 'nvim-treesitter/nvim-treesitter', { run =  ':TSUpdate' })
 	use('nvim-treesitter/playground')
