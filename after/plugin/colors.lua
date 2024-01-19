72bf63cbb9b166ec1b0cf666888a4bf4bf46bf6c
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2024-01-19 07:41:37 -0300

    atualiza coloschemes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,8 +5,38 @@ require("rose-pine").setup({
     italic = true,
     transparency = false
   },
-  headings = "subtle"
-})
+  groups = {
+        border = "muted",
+        link = "iris",
+        panel = "surface",
+
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        warn = "gold",
+
+        git_add = "foam",
+        git_change = "rose",
+        git_delete = "love",
+        git_dirty = "rose",
+        git_ignore = "muted",
+        git_merge = "iris",
+        git_rename = "pine",
+        git_stage = "iris",
+        git_text = "rose",
+        git_untracked = "subtle",
+
+        headings = {
+            h1 = "iris",
+            h2 = "foam",
+            h3 = "rose",
+            h4 = "gold",
+            h5 = "pine",
+            h6 = "foam",
+        },
+    }
+  }
+)
 
 
 function ColorMyPencils(color)
