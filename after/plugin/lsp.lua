2227b28fb9b342a40469fc019235d97eae6d30e7
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2024-01-30 16:17:31 -0300

    lsp markdown

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -33,8 +33,9 @@ lspconfig.tsserver.setup({
 })
 
 lspconfig.tailwindcss.setup({capabilities = lsp_capabilities})
+lspconfig.marksman.setup({capabilities = lsp_capabilities})
 
-local null_ls = require('null-ls')
+null_ls = require('null-ls')
 
 local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
 local event = "BufWritePre"
